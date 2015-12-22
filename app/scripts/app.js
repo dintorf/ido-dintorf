0b921bb61bc60c545a8c2b4fad74113405191c66
dintorf <dintorf@gmail.com>
2015-12-21 21:23:48 -0700

    added angular fire project starter

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name idodintorfcomApp
+ * @description
+ * # idodintorfcomApp
+ *
+ * Main module of the application.
+ */
+angular.module('idodintorfcomApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name idodintorfcomApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the idodintorfcomApp
+ */
+angular.module('idodintorfcomApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
