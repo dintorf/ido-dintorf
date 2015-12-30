ea5b5bf4c9f2697e5e9c89ea49819a3898372fbe
dintorf <dintorf@gmail.com>
2015-12-29 22:18:18 -0700

    added angular material and stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,15 +9,37 @@
  * Main module of the application.
  */
 angular.module('idodintorfcomApp', [
-    'ngAnimate',
-    'ngAria',
+    'ngMaterial',
     'ngCookies',
-    'ngMessages',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch',
     'firebase',
     'firebase.ref',
     'firebase.auth'
-  ]);
+  ])
+    .config(function($mdThemingProvider) {
+      // Extend the theme with a few different colors
+      var primary = $mdThemingProvider.extendPalette('deep-purple', {
+        '500': '39006A'
+      });
+      var accent = $mdThemingProvider.extendPalette('green', {
+        'A200': '85EC84',
+        'A700': '71B265',
+      });
+      var warn = $mdThemingProvider.extendPalette('pink', {
+        '500': 'B299B0',
+        'A100': 'CCBDB4'
+      });
+      // Register the new color palette map with the name <code>idodintorfPrimary</code>
+      $mdThemingProvider.definePalette('idodintorfPrimary', primary);
+      $mdThemingProvider.definePalette('idodintorfAccent', accent);
+      $mdThemingProvider.definePalette('idodintorfWarn', warn);
+
+      // Use that theme for the primary intentions
+      $mdThemingProvider.theme('default')
+        .primaryPalette('idodintorfPrimary')
+        .accentPalette('idodintorfAccent')
+        .warnPalette('idodintorfWarn')
+        .backgroundPalette('grey');
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,19 +8,13 @@
  * Controller of the idodintorfcomApp
  */
 angular.module('idodintorfcomApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
+  .controller('MainCtrl', function ($scope, $sce) {
     $scope.rsvp = {
       answer: 1
     };
 
-    $scope.$watch("rsvp.answer", function(newValue, oldValue) {
-      if(oldValue == "2"){
+    $scope.$watch('rsvp.answer', function(newValue, oldValue) {
+      if(oldValue === '2'){
         delete $scope.rsvp.plus1;
       }
     });
@@ -37,7 +31,20 @@ angular.module('idodintorfcomApp')
         event.preventDefault();
     });
 
+    $scope.trustSrc = function(src) {
+      return $sce.trustAsResourceUrl(src);
+    };
+
+    $scope.photos = [
+        '//splashbase.s3.amazonaws.com/unsplash/regular/photo-1430916273432-273c2db881a0%3Fq%3D75%26fm%3Djpg%26w%3D1080%26fit%3Dmax%26s%3Df047e8284d2fdc1df0fd57a5d294614d',
+        '//splashbase.s3.amazonaws.com/getrefe/regular/tumblr_nqune4OGHl1slhhf0o1_1280.jpg',
+        '//splashbase.s3.amazonaws.com/unsplash/regular/photo-1433959352364-9314c5b6eb0b%3Fq%3D75%26fm%3Djpg%26w%3D1080%26fit%3Dmax%26s%3D3b9bc6caa190332e91472b6828a120a4',
+        '//splashbase.s3.amazonaws.com/lifeofpix/regular/Life-of-Pix-free-stock-photos-moto-drawing-illusion-nabeel-1440x960.jpg',
+        '//splashbase.s3.amazonaws.com/lifeofpix/regular/Life-of-Pix-free-stock-photos-new-york-crosswalk-nabeel-1440x960.jpg',
+        '//splashbase.s3.amazonaws.com/lifeofpix/regular/Life-of-Pix-free-stock-photos-clothes-exotic-travel-nabeel-1440x960.jpg',
+      ];
+
     $('#galleryModal').on('show.bs.modal', function (e) {
-       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+       $('#galleryImage').attr('src',$(e.relatedTarget).data('src'));
     });
   });
